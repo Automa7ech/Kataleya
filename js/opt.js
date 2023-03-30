694fa3e21861808b46247040cce50005df9810f4
Automa7ech <127550756+Automa7ech@users.noreply.github.com>
2023-03-30 13:39:45 -0400

    Add files via upload

--- js/opt.js
+++ js/opt.js
@@ -3,10 +3,13 @@
      document.addEventListener('DOMContentLoaded',function(){
 
         var miImagen = document.querySelectorAll('.grupo img');
-        console.log(miImagen.length);
         for(let i = 0; i < miImagen.length; i++){
-            miImagen[i].style.height = 'auto';
+            miImagen[i].addEventListener('load',function(){
+                miImagen[i].style.height = 'auto';
+            });
         }
+        
+        
         
     });
 })();
