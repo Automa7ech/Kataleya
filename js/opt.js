07bf63832708bc1fea6fee037cd1fb355c26bfc1
Automa7ech <127550756+Automa7ech@users.noreply.github.com>
2023-04-02 19:30:46 -0400

    Add files via upload

--- js/opt.js
+++ js/opt.js
@@ -1,15 +1,29 @@
-(function(){
+(function() {
     'use strict';
-     document.addEventListener('DOMContentLoaded',function(){
+    document.addEventListener('DOMContentLoaded', function() {
 
         var miImagen = document.querySelectorAll('.grupo img');
-        for(let i = 0; i < miImagen.length; i++){
-            miImagen[i].addEventListener('load',function(){
+        for (let i = 0; i < miImagen.length; i++) {
+            miImagen[i].addEventListener('load', function() {
                 miImagen[i].style.height = 'auto';
             });
         }
         
-        
-        
+var yaCargada = flase;
+function miFunction(){
+    if(!yaCargada){
+        actualizarPagina();
+        yaCargada=true;
+    }
+}
+
+
+
+
+
     });
+
 })();
+function actualizarPagina(){
+    location.reload();
+}
