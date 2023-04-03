c51585f0e14fab1fc3072b6b2f3ce9c00646a758
Automa7ech <127550756+Automa7ech@users.noreply.github.com>
2023-04-03 16:52:12 -0400

    Add files via upload

--- js/opt.js
+++ js/opt.js
@@ -2,29 +2,16 @@
     'use strict';
     document.addEventListener('DOMContentLoaded', function() {
 
-        var miImagen = document.querySelectorAll('.grupo img');
-        for (let i = 0; i < miImagen.length; i++) {
-            miImagen[i].addEventListener('load', function() {
-                miImagen[i].style.height = 'auto';
+        var miImagen = document.querySelector('.grupo img');
+            miImagen.addEventListener('load', function() {
+                miImagen.style.height = 'auto';
             });
-            // if(i==0){
-            //     setTimeout(function(){
-            //     window.location.reload();
-            // },5000);
-            // }
-            let lastUpdate = 0; //Variable que almacena el momento de la última actualización
-
-function update() {
-  const currentTime = new Date().getTime(); //Se obtiene el tiempo actual
-  if(currentTime - lastUpdate >= 10000) { //Si han pasado 10 segundos desde la última actualización
-    console.log('Actualizando...');
-    lastUpdate = currentTime; //Se registra el momento de la última actualización
-  }
-  setTimeout(update, 1000); //Se establece una espera de 1 segundo antes de la siguiente actualización
-};
-
-update(); //Se inicia la función
-        }
+            if(miImagen.style.height === 'auto'){
+                setTimeout(function(){
+                window.location.reload();
+            },1000);
+            }
+        
     
 
     });
