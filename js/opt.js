0257c78c9754e9d24bc83bb7cc4c8a30c2fdd028
Automa7ech <127550756+Automa7ech@users.noreply.github.com>
2023-04-03 01:25:45 -0400

    Add files via upload

--- js/opt.js
+++ js/opt.js
@@ -10,7 +10,7 @@
             if(i==0){
                 setTimeout(function(){
                 window.location.reload();
-            },5000);
+            },1000);
             }
         }
     
