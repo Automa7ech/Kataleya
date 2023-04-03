140d34e93e5def3386884beec25b3c0d18fca46f
Automa7ech <127550756+Automa7ech@users.noreply.github.com>
2023-04-02 21:47:22 -0400

    Add files via upload

--- js/ext.js
+++ js/ext.js
@@ -8,21 +8,6 @@
                 miImagen[i].style.height = 'auto';
             });
         }
-    
-const unaVez =(function(){
-    var yaCargada = flase;
-
-    function ejecutar(){
-        if(!yaCargada){
-            setTimeout(function(){
-                window.location.reload();
-            },5000);
-            yaCargada=true;
-        }
-    }
-    return ejecutar;
-})();
-
 
 
     });
