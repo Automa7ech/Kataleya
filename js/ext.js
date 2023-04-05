af42db0a8c7967c47dad0203cb59dce64272186e
Automa7ech <127550756+Automa7ech@users.noreply.github.com>
2023-04-05 00:15:12 -0400

    Add files via upload

--- js/ext.js
+++ js/ext.js
@@ -1,4 +1,15 @@
+function accion(){
+    var ancla = document.getElementsByClassName('nav-enlace');
+    var vari = document.getElementsByClassName('abs');
+    for(var i = 0;  i < ancla.length; i++){
+        ancla[i].classList.toggle('desaparece');
+        
+
+    }
+    vari[0].classList.toggle('textura');
+}
 (function() {
+
     'use strict';
     document.addEventListener('DOMContentLoaded', function() {
 
@@ -8,6 +19,7 @@
                 miImagen[i].style.height = 'auto';
             });
         }
+        
 
 
     });
