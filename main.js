ea1866f6c3ac2187891df237c1346e4ac484c8f1
Ronny <127550756+Automa7ech@users.noreply.github.com>
2023-04-23 16:59:45 -0400

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-var map = L.map('mapa').setView([22.43821, -79.89615], 30);
-
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
-
-L.marker([22.43821, -79.89615]).addTo(map)
-    .bindPopup('<h1>Cafeteria <b>Kataleya</b></h1>')
-    .openPopup();
