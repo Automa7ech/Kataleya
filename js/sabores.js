aba129a1da892b6bb8e416e2601a0816def0328b
Ronny <127550756+Automa7ech@users.noreply.github.com>
2023-06-27 09:45:08 -0400

    Actualizar sabores.js

--- js/sabores.js
+++ js/sabores.js
@@ -5,7 +5,7 @@
       if(document.getElementById('sabores')){
         //Helados
       var nuevoTitulo = document.createElement("p");
-      var nuevoTexto = document.createTextNode("Leche condensada");
+      var nuevoTexto = document.createTextNode("Chocolate, Coco, Vainilla, Naranja Piña");
       nuevoTitulo.appendChild(nuevoTexto);
       var viejoNodo = document.querySelector("#sabores p");
       viejoNodo.parentNode.replaceChild(nuevoTitulo, viejoNodo);
@@ -14,7 +14,7 @@
       else if (document.getElementById("suero")) {
         //Suero
         var nuevoTitulo1 = document.createElement("p");
-        var nuevoTexto1 = document.createTextNode("Leche Condensada, Fresa ");
+        var nuevoTexto1 = document.createTextNode("Vainilla, Chocolate, Coco, Naranja Piña");
         nuevoTitulo1.appendChild(nuevoTexto1);
         var viejoNodo1 = document.querySelector("#suero p");
         viejoNodo1.parentNode.replaceChild(nuevoTitulo1, viejoNodo1);
@@ -23,7 +23,7 @@
       else if (document.getElementById("frappe")) {
         //Frappe
         var nuevoTitulo2 = document.createElement("p");
-        var nuevoTexto2 = document.createTextNode("Naranja, Fresa, Melon, Durazno, Guanábana, Manzana");
+        var nuevoTexto2 = document.createTextNode("Naranja, Fresa, Melon, Durazno, Guanábana, Manzana, Limón ");
         nuevoTitulo2.appendChild(nuevoTexto2);
         var viejoNodo2 = document.querySelector("#frappe p");
         viejoNodo2.parentNode.replaceChild(nuevoTitulo2, viejoNodo2);
@@ -32,7 +32,7 @@
       else if (document.getElementById("batido")) {
         //Batido
         var nuevoTitulo3 = document.createElement("p");
-        var nuevoTexto3 = document.createTextNode("Leche condensada ");
+        var nuevoTexto3 = document.createTextNode("Coco, Chocolate, Vainilla, Naranja Piña");
         nuevoTitulo3.appendChild(nuevoTexto3);
         var viejoNodo3 = document.querySelector("#batido p");
         viejoNodo3.parentNode.replaceChild(nuevoTitulo3, viejoNodo3);
@@ -41,7 +41,7 @@
       else if (document.getElementById("natural")) {
         //Batido Natural
         var nuevoTitulo4 = document.createElement("p");
-        var nuevoTexto4 = document.createTextNode("Mamey");
+        var nuevoTexto4 = document.createTextNode("Mamey, Mango");
         nuevoTitulo4.appendChild(nuevoTexto4);
         var viejoNodo4 = document.querySelector("#natural p");
         viejoNodo4.parentNode.replaceChild(nuevoTitulo4, viejoNodo4);
